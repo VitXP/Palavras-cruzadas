b0ce7a0237c125f0f8b90275d7ea8012b66535fd
vitXP <vitor.pereira@sptech.school>
2022-12-09 13:49:32 -0300

    Adição da navbar e do footer da cruzadinha

--- cruzadinha.js
+++ cruzadinha.js
@@ -1,15 +1,47 @@
+
+
 let usuario = []
 
 function start() {
-    
+
     let nome = in_nome.value
 
-    if(nome == '') {
-        erro.innerHTML = `Digite um nome para prosseguir`
+    if (nome == '') {
+        erro.innerHTML = `Digite o seu nome para prosseguir`
     } else {
         usuario.push(nome)
         erro.innerHTML = ``
         inicio.innerHTML = ``
+
+
+
+        Segunda_Tela.innerHTML =
+
+            `
+<body>
+<div class="barra-navbar">
+                <div class="img-footer2">
+                    <img src="./img/robo.png" alt="robo" width="88px" height="86px">
+                </div>
+                
+                <div class="usuario">
+                <h2>Ola, ${usuario[0]}</h2>
+            </div>
+
+                </div>
+
+</body>
+
+        <footer>
+            <div class="footer-segunda">
+                <div class="txt-footer2">
+                    <h2>Cruzadinha © 2022 . Todos os direitos reservados. Desenvolvido por Vitor Xavier e Viviana Marca
+                    </h2>
+                </div>
+            </div>
+        </footer>
+`
+
     }
 
 
